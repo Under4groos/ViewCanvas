29fc0b2b96e95392bc566066c26bfdbe1aa35c48
Макс Титов <hhit934@gmail.com>
2025-03-10 20:31:33 +0300

    Добавьте файлы проекта.

---
+++ ViewCanvas/View/VCanvas.xaml.cs
@@ -0,0 +1,138 @@
+﻿using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace ViewCanvas.View
+{
+    /// <summary>
+    /// Логика взаимодействия для VCanvas.xaml
+    /// </summary>
+    public partial class VCanvas : Canvas
+    {
+        private readonly MatrixTransform _transform = new MatrixTransform();
+        private Point _initialMousePosition;
+
+        private bool _dragging;
+        private UIElement? _selectedElement;
+        private Vector _draggingDelta;
+
+
+
+
+
+        public VCanvas()
+        {
+            InitializeComponent();
+
+        }
+
+
+
+        public float Zoomfactor { get; set; } = 1.1f;
+
+
+
+
+        protected override void OnMouseDown(MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Middle)
+            {
+                _initialMousePosition = _transform.Inverse.Transform(e.GetPosition(this));
+            }
+
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                if (Children.Contains((UIElement)e.Source))
+                {
+                    _selectedElement = (UIElement)e.Source;
+                    Point mousePosition = Mouse.GetPosition(this);
+                    double x = Canvas.GetLeft(_selectedElement);
+                    double y = Canvas.GetTop(_selectedElement);
+                    Point elementPosition = new Point(x, y);
+                    _draggingDelta = elementPosition - mousePosition;
+                }
+                _dragging = true;
+            }
+            base.OnMouseDown(e);
+        }
+
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            if (e.MiddleButton == MouseButtonState.Pressed)
+            {
+                Point mousePosition = _transform.Inverse.Transform(e.GetPosition(this));
+                Vector delta = Point.Subtract(mousePosition, _initialMousePosition);
+                var translate = new TranslateTransform(delta.X, delta.Y);
+                _transform.Matrix = translate.Value * _transform.Matrix;
+
+                foreach (UIElement child in Children)
+                {
+                    child.RenderTransform = _transform;
+                }
+            }
+
+            if (_dragging && e.LeftButton == MouseButtonState.Pressed)
+            {
+                double x = Mouse.GetPosition(this).X;
+                double y = Mouse.GetPosition(this).Y;
+
+                if (_selectedElement != null)
+                {
+                    Canvas.SetLeft(_selectedElement, x + _draggingDelta.X);
+                    Canvas.SetTop(_selectedElement, y + _draggingDelta.Y);
+                }
+            }
+
+            base.OnMouseMove(e);
+        }
+
+        protected override void OnPreviewMouseUp(MouseButtonEventArgs e)
+        {
+            _dragging = false;
+            _selectedElement = null;
+            base.OnPreviewMouseDown(e);
+        }
+
+        protected override void OnMouseWheel(MouseWheelEventArgs e)
+        {
+            zoom(e.Delta, Zoomfactor, Mouse.GetPosition(this));
+            base.OnMouseWheel(e);
+        }
+
+        //   zoom(-1, 2, new Point(ActualWidth / 2 - 400, ActualHeight / 2));
+        public void zoom(int Delta, float scaleFactor, Point mousePostion)
+        {
+
+
+
+            if (Delta < 0)
+            {
+                scaleFactor = 1f / scaleFactor;
+            }
+
+            //Point mousePostion = Mouse.GetPosition(this);// e.GetPosition(this);
+
+            Matrix scaleMatrix = _transform.Matrix;
+            scaleMatrix.ScaleAt(scaleFactor, scaleFactor, mousePostion.X, mousePostion.Y);
+            _transform.Matrix = scaleMatrix;
+            Debug.WriteLine(scaleFactor);
+
+            foreach (UIElement child in Children.AsParallel())
+            {
+                double x = Canvas.GetLeft(child);
+                double y = Canvas.GetTop(child);
+
+                double sx = x * scaleFactor;
+                double sy = y * scaleFactor;
+
+                Canvas.SetLeft(child, sx);
+                Canvas.SetTop(child, sy);
+
+                child.RenderTransform = _transform;
+            }
+        }
+
+    }
+}
